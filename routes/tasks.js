b41064e81522d5be1cf6a6458fdd3085bb9e634e
Miguel Briseño <miguel.briseno.bustos@gmail.com>
2022-04-21 17:07:21 -0500

    Listo el back para deploy

--- routes/tasks.js
+++ routes/tasks.js
@@ -14,7 +14,8 @@ router.post('/addTask', (req, res) => {
     let data = new Date()
     let vls = {
         task: req.body.task,
-        date: data
+        date: data,
+        taskStatus: 'PENDIENTE'
     }
     let query = `INSERT INTO tasks SET ?`
     mySqlConnect.query(query, vls, (err, rows) => {
@@ -38,6 +39,49 @@ router.post('/getTasks', (req, res) => {
         }
         else{
             res.json({status: true, data: rows})
+        }
+    })
+})
+
+// ENDPOINT PARA ELIMINAR LA TAREA INIDIVIDUAL
+router.post('/deleteTask', (req, res) => {
+    let query = `DELETE FROM tasks WHERE id = ${req.body.id}`
+    mySqlConnect.query(query, (err, rows) => {
+        if(err){
+            console.log(err)
+            res.json({status: false, error: "Server error 2345"})
+        }
+        else{
+            res.json({status: true})
+        }
+    })
+})
+
+// ENDPOINT PARA APROBAR LA TAREA
+router.post('/aproveTask', (req, res) => {
+    let query = `UPDATE tasks SET taskStatus = 'TERMINADA' WHERE id = ${req.body.id}`
+    mySqlConnect.query(query, (err, rows) => {
+        if(err){
+            console.log(err)
+            res.json({status: false, error: "Server error 3456"})
+        }
+        else{
+            res.json({status: true})
+        }
+    })
+})
+
+// ENDPOINT PARA ACTUALIZAR LA TAREA
+router.post('/updateTask', (req, res) => {
+    console.log(req.body)
+    let query = `UPDATE tasks SET task = '${req.body.task}' WHERE id = ${req.body.id}`
+    mySqlConnect.query(query, (err, rows) => {
+        if(err){
+            console.log(err)
+            res.json({status: false, error: "Server error 4567"})
+        }
+        else{
+            res.json({status: true})
         }
     })
 })
